2f59b765ec07435d869aea011f5ea5e18238d1f6
paulmunyao <paulmunyao094@gmail.com>
2021-12-22 11:33:40 +0300

    Adding javascript files

---
+++ app.js
@@ -0,0 +1,31 @@
+// //addition
+var add = function add(number1, number2) {
+    return number1 + number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter a number"));
+  alert(add(number1, number2));
+  
+  //subtraction
+  var subtract = function subtract(number1, number2) {
+    return number1 - number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter a number"));
+  alert(subtract(number1, number2));
+  
+  //multiplication for multiplication it's not a must you put an intiger
+  var multiply = function multiply(number1,number2) {
+      return number1 * number2;
+  };
+  var number1 = prompt("Enter a number:");
+  var number2 = prompt("Enter another a number");
+  alert(multiply(number1, number2));
+  
+  //division
+  var divide = function division(number1, number2) {
+      return number1 / number2;
+  };
+  var number1 = prompt("Enter a number");
+  var number2 =prompt("Enter another number");
+  alert(divide(number1, number2));
