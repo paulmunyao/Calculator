0c424992312c859610014dfb1c3c56c84e505d52
paulmunyao <paulmunyao094@gmail.com>
2022-01-07 02:16:10 +0300

    Add subtraction unit

--- work.js
+++ work.js
@@ -1,26 +1,14 @@
 // // //addition
 let add = function add(number1, number2) {
-    return number1 + number2;
-  };
-  jQuery("form#add").submit(function(event){
-    event.preventDefault();
+  return number1 + number2;
+};
+jQuery("form#add").submit(function (event) {
+  event.preventDefault();
   let number1 = parseInt(jQuery("#add1").val());
   let number2 = parseInt(jQuery("#add2").val());
-  alert(add(number1, number2));
-  });
-  
-
-
-
-
-
-
-
-
-
-
-
-
+  let result = add(number1, number2);
+  jQuery("#output").text(result);
+});
 
 //   //subtraction
 //   var subtract = function subtract(number1, number2) {
@@ -29,7 +17,7 @@ let add = function add(number1, number2) {
 //   var number1 = parseInt(prompt("Enter a number:"));
 //   var number2 = parseInt(prompt("Enter a number"));
 //   alert(subtract(number1, number2));
-  
+
 //   //multiplication for multiplication it's not a must you put an intiger
 //   var multiply = function multiply(number1,number2) {
 //       return number1 * number2;
@@ -37,7 +25,7 @@ let add = function add(number1, number2) {
 //   var number1 = prompt("Enter a number:");
 //   var number2 = prompt("Enter another a number");
 //   alert(multiply(number1, number2));
-  
+
 //   //division
 //   var divide = function division(number1, number2) {
 //       return number1 / number2;
